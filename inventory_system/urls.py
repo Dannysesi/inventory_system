87fec88d15c9a9537075d786b4d9463caa72e730
Dannysesi <daniel.olusesi20@gmail.com>
2023-10-10 22:21:18 +0100

    updated settings and url

--- inventory_system/urls.py
+++ inventory_system/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from inventory import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.base, name='base'),
     path('index/', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('item/<int:id>/', views.item_detail, name='item_detail'),
